cf791f9fab398d84b4dbbef5dfb2bd3aab42d225
nlunce <naterrl15@byui.edu>
2024-01-22 13:29:34 -0700

    w03

--- lesson_02/team/team.py
+++ lesson_02/team/team.py
@@ -26,7 +26,19 @@ from cse251 import *
 class Request_thread(threading.Thread):
     # TODO - Add code to make an API call and return the results
     # https://realpython.com/python-requests/
-    pass
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        self.response = {}
+        self.status_code = {}
+
+    def run(self):
+        response = requests.get(self.url)
+        self.status_code = response.status_code
+        if response.status_code == 200:
+            self.response = response.json()
+        else:
+            print("RESPONSE = ", response.status_code)
 
 
 class Deck:
@@ -36,12 +48,22 @@ class Deck:
         self.remaining = 52
 
     def reshuffle(self):
-        print("Reshuffle Deck")
+        # print("Reshuffle Deck")
         # TODO - add call to reshuffle
+        req = Request_thread(rf"https://deckofcardsapi.com/api/deck/{self.id}/shuffle/")
+        req.start()
+        req.join()
 
     def draw_card(self):
         # TODO add call to get a card
-        pass
+        req = Request_thread(rf"https://deckofcardsapi.com/api/deck/{self.id}/draw/")
+        req.start()
+        req.join()
+        if req.status_code == 200 and req.response != {}:
+            self.remaining = req.response["remaining"]
+            return req.response["cards"][0]["code"]
+        else:
+            return ""
 
     def cards_remaining(self):
         return self.remaining
@@ -58,12 +80,12 @@ if __name__ == "__main__":
     #        team_get_deck_id.py program once. You can have
     #        multiple decks if you need them
 
-    deck_id = "ENTER ID HERE"
+    deck_id = "h3gk2lny13ww"
 
     # Testing Code >>>>>
     deck = Deck(deck_id)
     for i in range(55):
         card = deck.draw_endless()
-        print(f"card {i + 1}: {card}", flush=True)
+        print(f"Card {i + 1}: {card}", flush=True)
     print()
     # <<<<<<<<<<<<<<<<<<
--- lesson_02/team/team_solution.py
+++ lesson_02/team/team_solution.py
@@ -15,8 +15,8 @@ import json
 # # Include cse 251 common Python files
 from cse251 import *
 
-class Request_Thread(threading.Thread):
 
+class Request_Thread(threading.Thread):
     def __init__(self, url):
         # Call the Thread class's init function
         # threading.Thread.__init__(self)
@@ -32,36 +32,32 @@ class Request_Thread(threading.Thread):
         if response.status_code == 200:
             self.response = response.json()
         else:
-            print('RESPONSE = ', response.status_code)
+            print("RESPONSE = ", response.status_code)
 
 
 class Deck:
-
     def __init__(self, deck_id):
         self.id = deck_id
         self.reshuffle()
         self.remaining = 52
 
-
     def reshuffle(self):
-        req = Request_Thread(rf'https://deckofcardsapi.com/api/deck/{self.id}/shuffle/')
+        req = Request_Thread(rf"https://deckofcardsapi.com/api/deck/{self.id}/shuffle/")
         req.start()
         req.join()
 
-
     def draw_card(self):
-        req = Request_Thread(rf'https://deckofcardsapi.com/api/deck/{self.id}/draw/')
+        req = Request_Thread(rf"https://deckofcardsapi.com/api/deck/{self.id}/draw/")
         req.start()
         req.join()
         if req.status_code == 200 and req.response != {}:
-            self.remaining = req.response['remaining']
-            return req.response['cards'][0]['code']
+            self.remaining = req.response["remaining"]
+            return req.response["cards"][0]["code"]
         else:
-            return ''
+            return ""
 
     def cards_remaining(self):
         return self.remaining
-
 
     def draw_endless(self):
         if self.remaining <= 0:
@@ -69,15 +65,14 @@ class Deck:
         return self.draw_card()
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # DONE: Run the program team_get_deck_id.py ONCE and insert the deck ID here.
-    deck_id = 'CODE'
+    deck_id = "h3gk2lny13ww"
 
     deck = Deck(deck_id)
 
     for i in range(55):
         card = deck.draw_endless()
-        print(f'Card {i + 1}: {card}', flush=True)
+        print(f"Card {i + 1}: {card}", flush=True)
 
     print()
