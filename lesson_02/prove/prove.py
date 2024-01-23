895eff33ff65fd8a22c44da339c06b496fa5927e
nlunce <naterrl15@byui.edu>
2024-01-22 18:23:11 -0700

    w02 prove

--- lesson_02/prove/prove.py
+++ lesson_02/prove/prove.py
@@ -2,7 +2,7 @@
 Course: CSE 251 
 Lesson: L02 Prove
 File:   prove.py
-Author: <Add name here>
+Author: Nathan
 
 Purpose: Retrieve Star Wars details from a server
 
@@ -47,31 +47,146 @@ import threading
 from cse251 import *
 
 # Const Values
-TOP_API_URL = 'http://127.0.0.1:8790'
+TOP_API_URL = "http://127.0.0.1:8790"
 
 # Global Variables
 call_count = 0
 
 
 # TODO Add your threaded class definition here
+class Request_thread(threading.Thread):
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        self.response = {}
+        self.status_code = {}
+
+    def run(self):
+        response = requests.get(self.url)
+        self.status_code = response.status_code
+        if response.status_code == 200:
+            self.response = response.json()
+        else:
+            print("RESPONSE = ", response.status_code)
 
 
 # TODO Add any functions you need here
+def get_film_details(top_api_urls, film_number):
+    global call_count
+    req = Request_thread(top_api_urls["films"] + film_number)
+    call_count += 1
+    req.start()
+    req.join()
+    film = req.response
+
+    film_details = {
+        "title": film["title"],
+        "director": film["director"],
+        "producer": film["producer"],
+        "release_date": film["release_date"],
+        "character_names": get_names(film["characters"]),
+        "planet_names": get_names(film["planets"]),
+        "starship_names": get_names(film["starships"]),
+        "vehicle_names": get_names(film["vehicles"]),
+        "species_names": get_names(film["species"]),
+    }
+
+    return film_details
+
+
+def get_names(url_list):
+    global call_count
+    threads = []
+    for e in url_list:
+        req = Request_thread(e)
+        threads.append(req)
+        call_count += 1
+        req.start()
+
+    for thread in threads:
+        thread.join()
+
+    data = [thread.response for thread in threads]
+
+    names = []
+
+    for e in data:
+        names.append(e["name"])
+
+    sorted_names = sorted(names)
+
+    return sorted_names
+
+
+def display_details(log, film_details):
+    global call_count
+    log.write(f"Title: {film_details['title']}")
+    log.write(f"Director: {film_details['director']}")
+    log.write(f"Producer: {film_details['producer']}")
+    log.write(f"Released: {film_details['release_date']}")
+
+    character_count = len(film_details["character_names"])
+    log.write()
+    log.write(f"Characters: {character_count}")
+    log.write(str(film_details["character_names"])[1:-1].replace("'", ""))
+    log.write()
+
+    planet_count = len(film_details["planet_names"])
+    log.write(f"Planets: {planet_count}")
+    log.write(str(film_details["planet_names"])[1:-1].replace("'", ""))
+    log.write()
+
+    starship_count = len(film_details["starship_names"])
+    log.write(f"Starships: {starship_count}")
+    log.write(str(film_details["starship_names"])[1:-1].replace("'", ""))
+    log.write()
+
+    vehicle_count = len(film_details["vehicle_names"])
+    log.write(f"Vehicles: {vehicle_count}")
+    log.write(str(film_details["vehicle_names"])[1:-1].replace("'", ""))
+    log.write()
+
+    species_count = len(film_details["species_names"])
+    log.write(f"Species: {species_count}")
+    log.write(str(film_details["species_names"])[1:-1].replace("'", ""))
+    log.write()
+
+    # # lists
+
+
+def create_threads(url_list):
+    thread_list = []  # Stores threads
+
+    # Creates threads and adds them to thread list
+    for url in url_list:
+        thread = Request_thread(url)
+        thread_list.append(thread)
+
+    return thread_list
 
 
 def main():
+    global call_count
     log = Log(show_terminal=True)
-    log.start_timer('Starting to retrieve data from the server')
+    log.start_timer("Starting to retrieve data from the server")
 
     # TODO Retrieve Top API urls
+    req = Request_thread(TOP_API_URL)
+    call_count += 1
+    req.start()
+    req.join()
+    top_api_urls = req.response
 
     # TODO Retrieve Details on film 6
+    desired_film = "6"
+    film_details = get_film_details(top_api_urls, desired_film)
 
     # TODO Display results
+    display_details(log, film_details)
 
-    log.stop_timer('Total Time To complete')
-    log.write(f'There were {call_count} calls to the server')
-    
+    log.stop_timer("Total Time To complete")
+    log.write(f"There were {call_count} calls to the server")
+
 
 if __name__ == "__main__":
     main()
