6d724a7816a5e328737a9e95fc2b2d70d668cfaa
Nathan Lunceford <nathanlunceford2@gmail.com>
2024-04-07 00:21:11 -0600

    w14 prove pt1

--- lesson_14/prove/functions.py
+++ lesson_14/prove/functions.py
@@ -1,7 +1,7 @@
 """
 Course: CSE 251, week 14
 File: functions.py
-Author: Nathan Lunceford
+Author: <your name>
 
 Instructions:
 
@@ -62,12 +62,69 @@ import queue
 
 
 # -----------------------------------------------------------------------------
-def depth_fs_pedigree(family_id, tree):
+# Function to recursively process people
+def recursive_process_person(person_id, tree, processed_families):
+    if tree.does_person_exist(person_id):
+        return
+
+    # Create threaded request for person's data
+    person_request = Request_thread(f"{TOP_API_URL}/person/{person_id}")
+    person_request.start()
+    person_request.join()
+
+    # Extract and process person data
+    person_json = person_request.get_response()
+    if person_json:
+        person_obj = Person(person_json)
+        tree.add_person(person_obj)
+
+        # Depth-first recursion for the person's parental family
+        parent_family_id = person_obj.get_parentid()
+        if parent_family_id and parent_family_id not in processed_families:
+            depth_fs_pedigree(parent_family_id, tree, processed_families)
+
+
+def depth_fs_pedigree(family_id, tree, processed_families=set()):
     # KEEP this function even if you don't implement it
     # TODO - implement Depth first retrieval
     # TODO - Printing out people and families that are retrieved from the server will help debugging
 
-    pass
+    if family_id in processed_families:
+        return
+
+    processed_families.add(family_id)
+
+    family_request = Request_thread(f"{TOP_API_URL}/family/{family_id}")
+    family_request.start()
+    family_request.join()
+
+    family_json = family_request.get_response()
+    if family_json:
+        family_obj = Family(family_json)
+        if not tree.does_family_exist(family_obj.get_id()):
+            tree.add_family(family_obj)
+
+        # Collect all family member IDs, including husband, wife, and children
+        family_members_ids = [
+            family_obj.get_husband(),
+            family_obj.get_wife(),
+            *family_obj.get_children(),
+        ]
+
+        # Create and start threads for each family member
+        person_threads = []
+        for person_id in family_members_ids:
+            if person_id:
+                thread = threading.Thread(
+                    target=recursive_process_person,
+                    args=(person_id, tree, processed_families),
+                )
+                thread.start()
+                person_threads.append(thread)
+
+        # Join threads
+        for thread in person_threads:
+            thread.join()
 
 
 # -----------------------------------------------------------------------------
